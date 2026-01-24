0fba94a2a06d799db0a7f464f1aab88209600625
Keith Edwards <89081584+kedwards1306@users.noreply.github.com>
2026-01-24 11:16:35 -0500

    Add PastScores in the menu

--- kedwards.MathGame/Program.cs
+++ kedwards.MathGame/Program.cs
@@ -39,6 +39,7 @@ string UserChoices()
     Console.WriteLine("2. Subtraction");
     Console.WriteLine("3. Multiplication");
     Console.WriteLine("4. Division");
+    Console.WriteLine("5. ShowPastScores");
 
     string choice = Console.ReadLine();
 
